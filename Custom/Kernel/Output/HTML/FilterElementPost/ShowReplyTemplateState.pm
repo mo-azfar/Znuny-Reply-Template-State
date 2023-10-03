b5db7228ca02859675360d8a55abbbda30f01d97
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2023-10-03 13:34:10 +0800

    new release

---
+++ Custom/Kernel/Output/HTML/FilterElementPost/ShowReplyTemplateState.pm
@@ -0,0 +1,113 @@
+# --
+# Copyright (C) 2023 mo-azfar,https://github.com/mo-azfar
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::FilterElementPost::ShowReplyTemplateState;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::Output::HTML::Layout',
+    'Kernel::System::Web::Request',
+);
+
+use Kernel::System::VariableCheck qw(:all);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject                    = $Kernel::OM->Get('Kernel::Config');
+    my $GroupObject                     = $Kernel::OM->Get('Kernel::System::Group');
+    my $LayoutObject                    = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ParamObject                     = $Kernel::OM->Get('Kernel::System::Web::Request');
+   
+	my $Action = $ParamObject->GetParam( Param => 'Action' );
+	
+	return 1 if !$Action;
+    return 1 if !$Param{Templates}->{$Action};
+	return 1 if !$Param{ResponseTemplateDefaultState};
+	
+	my $StandardTemplateObject = $Kernel::OM->Get('Kernel::System::StandardTemplate');
+	my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+	my $TicketID = $ParamObject->GetParam( Param => 'TicketID' );
+	my $ResponseID = $ParamObject->GetParam( Param => 'ResponseID' );
+	
+	my $StateGenerate = 0;
+	my $NextStateName = 0;
+	my $NextStateID = 0;
+	
+    StateAtt:
+	
+	#compare config with current template screen id
+	foreach my $TemplateName ( keys %{$Param{ResponseTemplateDefaultState}} )
+	{
+		my $StandardTemplateID = $StandardTemplateObject->StandardTemplateLookup(
+			StandardTemplate => $TemplateName,
+		);
+		
+		if ( $ResponseID eq $StandardTemplateID ) {
+            			
+			$NextStateName = $Param{ResponseTemplateDefaultState}{$TemplateName};
+			$NextStateID = $Kernel::OM->Get('Kernel::System::State')->StateLookup(
+			    State => $NextStateName,
+			);
+			
+			if ( $NextStateID )
+			{
+				$StateGenerate = 1;
+			}
+			
+            last StateAtt;
+        }
+	}
+	
+	if ( $StateGenerate )
+	{	
+		my %PossibleNextStates;
+		$PossibleNextStates{$NextStateID} = $NextStateName;
+		
+		my %Ticket = $TicketObject->TicketGet(
+		    TicketID      => $TicketID,
+		    UserID        => 1,
+		);
+
+		#new state selection based on reply template state config
+		my $NewOutput = $LayoutObject->BuildSelection(
+			Data            => \%PossibleNextStates, 
+			Name            => 'StateID',
+			PossibleNone    => 1,
+			Class           => 'Modernize',
+			SelectedID      => $NextStateID,
+			SelectedValue   => $PossibleNextStates{$NextStateID},
+			Title 			=> $Ticket{Title},
+		);
+		
+		my $SearchFieldStart = quotemeta "<select class=\"Modernize\" id=\"StateID\" name=\"StateID\" title=\"$Ticket{Title}\">";
+		my $SearchFieldEnd = quotemeta "</select>";
+		my $ReturnField = qq~$NewOutput
+		~;
+		
+		#search and replace	 
+		${ $Param{Data} } =~ s{$SearchFieldStart[\d\D]*?$SearchFieldEnd}{$ReturnField};
+		
+	}
+	
+    return 1;
+}
+
+1;
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Znuny-Reply-Template-State
- set ticket state based on selected compose / reply template
+- set ticket state based on selected compose / answer / reply template
+
+1. Update 'ResponseTemplateDefaultState'. Go to Admin > System Configuration > Frontend::Output::FilterElementPost###ShowReplyTemplateState
+
+		ResponseTemplateDefaultState 
+			
+			Template Name => State Name
+			
+		*Where Template Name = response answer template name ( Admin > Templates )
+		*Where State Name = ticket state name ( Admin > States )
+			
+		Example,
+			
+			Closed Ticket => closed successful
+			Waiting for Customer Response => pending auto close+
+			
+		*Where Template name = 'Closed Ticket', selected ticket state will be 'closed successful'
+		*Where Template name = 'Waiting for Customer Response', selected ticket state will be 'pending auto close+'
+		
